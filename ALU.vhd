fbd413c1cd66e423eed5e6758ee9bb902b5a403e
Titouan Bresque <titouan.bresque@student-cs.fr>
2023-01-18 16:12:33 +0100

    added switch modif in Register

--- ALU.vhd
+++ ALU.vhd
@@ -6,8 +6,8 @@ entity ALU is
     port (
         a, b, c: in std_logic_vector(7 downto 0); -- a and b are the inputs from the register, c is the direct one from the decoder
         op: in std_logic_vector(2 downto 0);
-        result: out std_logic_vector(7 downto 0) -- result is directly put according to the write adress coming out of the decoder
-		  w_enable : out std_logic;
+        result: out std_logic_vector(7 downto 0); -- result is directly put according to the write adress coming out of the decoder
+		  w_enable : out std_logic
     );
 end ALU;		
 
@@ -18,7 +18,7 @@ begin
         case op is
             when "000" => result <= std_logic_vector(unsigned(a) + unsigned(c));
             when "001" => result <= std_logic_vector(unsigned(a) - unsigned(c));
-				when "010" => result(0) <= a(unsigned(b)) xnor c(0); -- checks if the floor is called :  a:call list, b:current floor, c:1 to check if florr if called 0 else
+				when "010" => result(0) <= a(to_integer(unsigned(b))) xnor c(0); -- checks if the floor is called :  a:call list, b:current floor, c:1 to check if florr if called 0 else
             when "011" => result <= c;
             when "100" => result(0) <= b(0) and((b(1) and (not b(2)) and a(7)) or ((not b(1)) and ((a(6) or a(7)) or ((not b(2)) and a(5))))) or
 				(not b(0)) and ((a(4) or a(5) or a(6) or a(7)) or ((not(b1)) and ((a(2) or a(3)) or ((not b(2)) and a(1)))) or (b(1) and a(3)));
@@ -27,9 +27,9 @@ begin
             when "111" => result <= a xnor c; 
         end case;
 		  if op = '101' or op='110' then
-				w_enable <= '0'
+				w_enable <= '0';
 		  else
-				w_enable <= '1'
+				w_enable <= '1';
 		  end if;
     end process;
 end ALU_arch;
--- Register.vhd
+++ Register.vhd
@@ -6,17 +6,22 @@ Use ieee.numeric_std.all ;
 
 entity reg is
 	port(
-			rw,en		:	in std_logic;
+			en		:	in std_logic;
+			w_enable :  in std_logic;
 			clk		:	in std_logic;
-			Adress	:	in std_logic_vector(7 downto 0);
+			Address_w	:	in std_logic_vector(3 downto 0);
+			Address_r_1 :  in std_logic_vector(3 downto 0); --address of a
+			Address_r_2 :  in std_logic_vector(3 downto 0); --address of b
 			Data_in	:	in std_logic_vector(7 downto 0);
-			Data_out:	out std_logic_vector(7 downto 0)
+			SW :  IN  STD_LOGIC_VECTOR(9 DOWNTO 0);
+			Data_out_1:	out std_logic_vector(7 downto 0); -- value a in ALU
+			Data_out_2:	out std_logic_vector(7 downto 0); -- value b in ALU
 			);
 end reg;
 
 architecture register_arch of reg is
 
-type reg is array(0 to 7) of std_logic_vector(15 downto 0);
+type reg is array(0 to 7) of std_logic_vector(7 downto 0);
 
 signal Data_reg : reg ;
 
@@ -24,17 +29,45 @@ signal Data_reg : reg ;
 
 --------------- BEGIN -----------------------------------------------------------------
 begin
--- rw='1' alors lecture
+
 	acces_reg:process(rst, clk)
 		begin
 		
 		if rising_edge(clk) then
+			Data_reg(to_integer(unsigned('000'))) <= std_logic_vector(unsigned(Data_reg(to_integer(unsigned('000'))) + 1)) -- implementation du program counter
 			if en='1' then
-				if(rw='1') then 
-					Data_out <= Data_reg(to_integer(unsigned(Adress)));
-				else
-					Data_reg(to_integer(unsigned(Adress))) <= Data_in;
-				end if;
+				Data_out_1 <= Data_reg(to_integer(unsigned(Address_r_1)));
+				Data_out_2 <= Data_reg(to_integer(unsigned(Address_r_2)));
+				if w_enable='1' then
+					Data_reg(to_integer(unsigned(Adress_w))) <= Data_in;
+				enf if;
+			end if;
+			if rising_edge(SW(0)) then
+				Data_reg(to_integer(unsigned('001')))(5) <= '0';
+			end if;
+			if rising_edge(SW(1)) then
+				Data_reg(to_integer(unsigned('010')))(0) <= '1';
+			end if;
+			if rising_edge(SW(2)) then
+				Data_reg(to_integer(unsigned('010')))(1) <= '1';
+			end if;
+			if rising_edge(SW(3)) then
+				Data_reg(to_integer(unsigned('010')))(2) <= '1';
+			end if;
+			if rising_edge(SW(4)) then
+				Data_reg(to_integer(unsigned('010')))(3) <= '1';
+			end if;
+			if rising_edge(SW(5)) then
+				Data_reg(to_integer(unsigned('010')))(4) <= '1';
+			end if;
+			if rising_edge(SW(6)) then
+				Data_reg(to_integer(unsigned('010')))(5) <= '1';
+			end if;
+			if rising_edge(SW(7)) then
+				Data_reg(to_integer(unsigned('010')))(6) <= '1';
+			end if;
+			if rising_edge(SW(8)) then
+				Data_reg(to_integer(unsigned('010')))(7) <= '1';
 			end if;
 		end if;
 		
