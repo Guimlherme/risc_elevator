a43d0eaeab88cdc7c8bb85d21e1397f94e21c4c5
Titouan Bresque <titouan.bresque@student-cs.fr>
2023-01-18 17:26:28 +0100

    debugging

--- ALU.vhd
+++ ALU.vhd
@@ -20,13 +20,13 @@ begin
             when "001" => result <= std_logic_vector(unsigned(a) - unsigned(c));
 				when "010" => result(0) <= a(to_integer(unsigned(b))) xnor c(0); -- checks if the floor is called :  a:call list, b:current floor, c:1 to check if florr if called 0 else
             when "011" => result <= c;
-            when "100" => result(0) <= b(0) and((b(1) and (not b(2)) and a(7)) or ((not b(1)) and ((a(6) or a(7)) or ((not b(2)) and a(5))))) or
-				(not b(0)) and ((a(4) or a(5) or a(6) or a(7)) or ((not(b1)) and ((a(2) or a(3)) or ((not b(2)) and a(1)))) or (b(1) and a(3)));
+            when "100" => result(0) <= ((b(0) and ((b(1) and (not b(2)) and a(7)) or ((not b(1)) and (((a(6)) or (a(7))) or ((not b(2)) and a(5))))))or 
+				((not b(0)) and ((a(4) or a(5) or a(6) or a(7)) or ((not(b(1))) and ((a(2) or a(3)) or ((not b(2)) and a(1)))) or (b(1) and a(3)))));
             when "101" => result <= c;
 				when "110" => result <= c; 
             when "111" => result <= a xnor c; 
         end case;
-		  if op = '101' or op='110' then
+		  if ((op = "101") or (op="110")) then
 				w_enable <= '0';
 		  else
 				w_enable <= '1';
